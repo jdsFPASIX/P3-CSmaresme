7219139c98f67314026d31c2fd965dbd8000665f
jdsFPASIX <30305606r@iespoblenou.org>
2023-02-06 20:03:38 +0100

    commit 1

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world(): # put application's code here
     return 'Hello World!'
 
 
