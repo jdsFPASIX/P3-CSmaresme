be643d444ac244cad4fb624dd3c871d11578a2e8
jdsFPASIX <30305606r@iespoblenou.org>
2023-02-09 17:17:02 +0100

    commit daniel

--- app.py
+++ app.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world(): # put application's code here
-
     return render_template('template_index.html')
 
 
-"""if __name__ == '__main__':
-    app.run(debug='True')"""
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('Contacto.html')
+
+
+if __name__ == '__main__':
+    app.run(debug='True')
 
 
 
