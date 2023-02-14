93474eb1ae724900475898c33dbd2b1d9a703ecf
jdsFPASIX <30305606r@iespoblenou.org>
2023-02-14 17:03:13 +0100

    sincronizacion

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world(): # put application's code here
-    return render_template('template_index.html')
+    return render_template('index.html')
 
 
 @app.route('/login')
