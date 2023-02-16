0c622a6f5db0d47780c4666d6747dc630937edec
miqueasgonzalez <gonzalezmiqueas55@gmail.com>
2023-02-16 15:57:33 +0100

    REGISTER y url

--- app.py
+++ app.py
@@ -17,19 +17,23 @@ def login():
 def contacto():
     return render_template('Contacto.html')
 
-@app.route('/register', methods=['post'])
+
+@app.route('/register', methods=['GET','POST'])
 def crearusuario():
-    nombre = request.form['nombre']
-    apellido1 = request.form['apellido1']
-    apellido2 = request.form['apellido2']
-    fecha_nac = request.form['fecha_nac']
-    email = request.form['email']
-    telefono = request.form['telefono']
-    usuario = request.form['usuario']
-    password = request.form['password']
-    funcionesbbdd.nuevousuario(nombre,apellido1,apellido2,fecha_nac,email,telefono,usuario,password)
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        nombre = request.form['nombre']
+        apellido1 = request.form['apellido1']
+        apellido2 = request.form['apellido2']
+        fecha_nac = request.form['fecha_nac']
+        email = request.form['email']
+        telefono = request.form['telefono']
+        usuario = request.form['usuario']
+        password = request.form['password']
+        funcionesbbdd.nuevousuario(nombre,apellido1,apellido2,fecha_nac,email,telefono,usuario,password)
     #return redirect('/register')
-    return 'OK'
+    return redirect(url_for('/'))
 
 
 if __name__ == '__main__':
